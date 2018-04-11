97bd457bfd5d2ef305f80ef1018d7108562b950f
Daniel Peters <daniel.peters.ch@gmail.com>
2018-04-11 21:22:15 +0200

    Update server and dependencies.

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
+const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
-const favicon = require('serve-favicon')
-const logger = require('morgan')
 const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
+const logger = require('morgan')
 const sassMiddleware = require('node-sass-middleware')
 
-const index = require('./routes/index')
-const users = require('./routes/users')
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
 
 const app = express()
 
@@ -15,11 +14,9 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
@@ -29,18 +26,14 @@ app.use(sassMiddleware({
 }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', index)
-app.use('/users', users)
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
-app.use( (req, res, next) => {
-  const err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
+app.use((req, res, next) => next(createError(404)))
 
 // error handler
-app.use( (err, req, res, next) => {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
